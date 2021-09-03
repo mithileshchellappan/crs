dadd11df1e8863c8f6ef756bf2ffefe37cb9a513
mithileshchellappan <35962266+mithileshchellappan@users.noreply.github.com>
2021-09-03 22:38:46 +0530

    2 presence and clear channnel

--- dist/index.dev.js
+++ dist/index.dev.js
@@ -1,17 +1,43 @@
 "use strict";
 
-var Discord = require('discord.js');
+var Discord = require("discord.js");
 
 var client = new Discord.Client();
 
-var config = require('./config.json');
+var config = require("./config.json");
 
-var command = require('./commands');
+var command = require("./commands");
 
-client.on('ready', function () {
-  console.log('BOT READY');
-  command(client, ['ping', 'test'], function (message) {
-    message.channel.send('pong');
+client.on("ready", function () {
+  console.log("BOT READY");
+  command(client, ["ping", "test"], function (message) {
+    message.channel.send("pong");
+  });
+  command(client, "servers", function (message) {
+    client.guilds.cache.forEach(function (guild) {
+      message.channel.send("".concat(guild.name, " has a total of ").concat(guild.memberCount, " mem"));
+    });
+  });
+  command(client, ['cc', 'clearchannel'], function (message) {
+    if (message.member.hasPermission('ADMINISTRATOR')) {
+      message.channel.messages.fetch().then(function (results) {
+        message.channel.bulkDelete(results);
+      });
+    }
+  });
+  command(client, 'status', function (message) {
+    var content = message.content.replace('!status', '');
+
+    if (content !== '') {
+      client.user.setPresence({
+        activity: {
+          name: content,
+          type: 0
+        }
+      });
+    } else {
+      message.channel.send('Type something');
+    }
   });
 });
 client.login(config.token);
--- index.js
+++ index.js
@@ -1,15 +1,45 @@
-const Discord = require('discord.js')
-const client = new Discord.Client()
-const config = require('./config.json')
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const config = require("./config.json");
 
-const command = require('./commands')
+const command = require("./commands");
 
-client.on('ready',()=>{
-    console.log('BOT READY')
+client.on("ready", () => {
+  console.log("BOT READY");
 
-    command(client,['ping','test'],message =>{
-        message.channel.send('pong')
-    })
-})
+  command(client, ["ping", "test"], (message) => {
+    message.channel.send("pong");
+  });
 
-client.login(config.token)
+  command(client, "servers", (message) => {
+    client.guilds.cache.forEach((guild) => {
+      message.channel.send(
+        `${guild.name} has a total of ${guild.memberCount} mem`
+      );
+    });
+  });
+
+  command(client,['cc','clearchannel'],message=>{
+      if(message.member.hasPermission('ADMINISTRATOR')){
+          message.channel.messages.fetch().then(results=>{message.channel.bulkDelete(results)})
+      }
+  })
+
+  command(client,'status',message=>{
+      const content = message.content.replace('!status','')
+
+    if(content!==''){
+        client.user.setPresence({
+            activity:{
+                name:content,
+                type:0
+            }
+        })
+    }else{
+        message.channel.send('Type something')
+    }
+  })
+
+});
+
+client.login(config.token);
