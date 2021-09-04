1eee4e4d25e33b27f40cce00a14e925543a26924
mithileshchellappan <35962266+mithileshchellappan@users.noreply.github.com>
2021-09-04 16:33:17 +0530

    roles based on reaction

--- dist/index.dev.js
+++ dist/index.dev.js
@@ -6,18 +6,12 @@ var client = new Discord.Client();
 
 var config = require("./config.json");
 
+var roleAssign = require('./role-assign');
+
 var command = require("./commands");
 
 client.on("ready", function () {
   console.log("BOT READY");
-  command(client, "help", function (message) {
-    message.channel.send("**!help** - Displays help menu\n  **!add** <num1> <num2> - Adds 2 numbers\n  **!sub** <num1> <num2> -Subs 2 numbers\n    ");
-  });
-  var prefix = config.prefix;
-  client.user.setPresence({
-    activity: {
-      name: "Use ".concat(prefix, " help")
-    }
-  });
+  roleAssign(client);
 });
 client.login(config.token);
--- index.js
+++ index.js
@@ -1,26 +1,12 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
-
+const roleAssign = require('./role-assign')
 const command = require("./commands");
 
 client.on("ready", () => {
   console.log("BOT READY");
-
-  command(client, "help", (message) => {
-    message.channel.send(`**!help** - Displays help menu
-  **!add** <num1> <num2> - Adds 2 numbers
-  **!sub** <num1> <num2> -Subs 2 numbers
-    `);
-  });
-
-const {prefix} = config
-client.user.setPresence({
-  activity:{
-    name:`Use ${prefix} help`
-  }
-})
-
+   roleAssign(client)
 });
 
 client.login(config.token);
