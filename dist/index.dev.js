7b19ac2b377d41e6e2be481addbc89c696eedfe3
mithileshchellappan <35962266+mithileshchellappan@users.noreply.github.com>
2021-09-04 16:49:21 +0530

    kick and ban

--- dist/index.dev.js
+++ dist/index.dev.js
@@ -6,12 +6,45 @@ var client = new Discord.Client();
 
 var config = require("./config.json");
 
-var roleAssign = require('./role-assign');
-
 var command = require("./commands");
 
 client.on("ready", function () {
   console.log("BOT READY");
-  roleAssign(client);
+  command(client, "ban", function (message) {
+    var member = message.member,
+        mentions = message.mentions;
+
+    if (member.hasPermission("ADMINISTRATOR") || member.hasPermission("BAN_MEMBERS")) {
+      var target = mentions.users.first();
+
+      if (target) {
+        var targetMember = message.guild.members.cache.get(target.id);
+        targetMember.ban();
+        message.channel.send('Banned successfully');
+      } else {
+        message.channel.send('Specify to ban');
+      }
+    } else {
+      message.channel.send("<@".concat(member.id, "> You do not have permission to use this command."));
+    }
+  });
+  command(client, "kick", function (message) {
+    var member = message.member,
+        mentions = message.mentions;
+
+    if (member.hasPermission("ADMINISTRATOR") || member.hasPermission("KICK_MEMBERS")) {
+      var target = mentions.users.first();
+
+      if (target) {
+        var targetMember = message.guild.members.cache.get(target.id);
+        targetMember.kick();
+        message.channel.send('Kicked successfully');
+      } else {
+        message.channel.send('Specify to kick');
+      }
+    } else {
+      message.channel.send("<@".concat(member.id, "> You do not have permission to use this command."));
+    }
+  });
 });
 client.login(config.token);
--- index.js
+++ index.js
@@ -1,12 +1,54 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
-const roleAssign = require('./role-assign')
 const command = require("./commands");
 
 client.on("ready", () => {
   console.log("BOT READY");
-   roleAssign(client)
+
+  command(client, "ban", (message) => {
+    const { member, mentions } = message;
+    
+    if (
+      member.hasPermission("ADMINISTRATOR") ||
+      member.hasPermission("BAN_MEMBERS")
+    ) {
+      const target = mentions.users.first()
+      if(target){
+        const targetMember = message.guild.members.cache.get(target.id)
+        targetMember.ban()
+        message.channel.send('Banned successfully')
+      }else{
+        message.channel.send('Specify to ban')
+      }
+    } else {
+      message.channel.send(
+        `<@${member.id}> You do not have permission to use this command.`
+      );
+    }
+  });
+
+  command(client, "kick", (message) => {
+    const { member, mentions } = message;
+    
+    if (
+      member.hasPermission("ADMINISTRATOR") ||
+      member.hasPermission("KICK_MEMBERS")
+    ) {
+      const target = mentions.users.first()
+      if(target){
+        const targetMember = message.guild.members.cache.get(target.id)
+        targetMember.kick()
+        message.channel.send('Kicked successfully')
+      }else{
+        message.channel.send('Specify to kick')
+      }
+    } else {
+      message.channel.send(
+        `<@${member.id}> You do not have permission to use this command.`
+      );
+    }
+  });
 });
 
 client.login(config.token);
