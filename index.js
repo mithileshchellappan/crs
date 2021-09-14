df57fdbfe662f8259a7d62e45aad18ad342c266c
mithileshchellappan <35962266+mithileshchellappan@users.noreply.github.com>
2021-09-14 11:40:08 +0530

    adv polls

--- index.js
+++ index.js
@@ -7,11 +7,13 @@ const config = require("./config.json");
 
 const loadCommands = require('./commands/load-commands')
 
+const advancePolls = require('./advancePolls')
+
 client.on("ready", async () => {
   console.log("BOT READY!");
 
   loadCommands(client);
-  
+  advancePolls(client)
 });
 
 client.login(config.token);
